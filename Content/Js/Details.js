29a508a1aa1c965f2e39ae1e76f373e5c0d5e905
cleo-cyber <kadimacleo92@gmail.com>
2023-04-09 00:17:25 +0300

    change listing detail page

---
+++ Content/Js/Details.js
@@ -0,0 +1,9 @@
+﻿const accordion = document.querySelectorAll('.opener');
+const openContent = document.querySelectorAll('.content');
+accordion.forEach((accordion) => {
+    accordion.addEventListener('click', () => {
+        openContent.forEach((item) => {
+            item.classList.toggle('open')
+        })
+    })
+})
