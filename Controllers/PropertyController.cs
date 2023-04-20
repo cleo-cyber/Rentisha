ca154445832ff5935040b852d46e1a9745807194
wangui <86366610+awangui@users.noreply.github.com>
2023-04-20 22:49:01 +0300

    Login and sign up styling delete functionality

--- Controllers/PropertyController.cs
+++ Controllers/PropertyController.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Rentisha.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -14,28 +18,108 @@ namespace Rentisha.Controllers
         {
             return View();
         }
-
+        [HttpGet]
         public ActionResult Listings()
         {
-            return View();
+            using (KodishaEntities2 dc = new KodishaEntities2())
+            {
+                List<Property> props = dc.Properties.ToList();
+                return View(props);
+            }
+
         }
+
+
 
         public ActionResult AddListings()
         {
 
             return View();
         }
-        public ActionResult ListingDetails()
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddListings(Property prop)
+        {
+            if (ModelState.IsValid)
+            {
+                using (KodishaEntities2 dc = new KodishaEntities2())
+                {
+                    dc.Properties.Add(prop);
+                    dc.SaveChanges();
+                }
+
+            }
+            return RedirectToAction("Listings");
+        }
+        public ActionResult ListingDetail()
         {
             return View();
         }
-        public ActionResult EditListing() 
+
+        [HttpGet]
+        public ActionResult EditListing(int property_id)
         {
-            return View();
+            using (var dc = new KodishaEntities2())
+            {
+                Property prop = dc.Properties.Find(property_id);
+                if (prop != null)
+                {
+                    return View(prop);
+                }
+                else
+                {
+                    ViewBag.Message = "Property not found";
+                }
+            }
+
+            return RedirectToAction("Listings");
+
         }
-        public ActionResult DeleteListing()
+        
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditListing(Property prop)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var dc = new KodishaEntities2())
+                {
+                    dc.Properties.Attach(prop);
+                    dc.Entry(prop).State = System.Data.Entity.EntityState.Modified;
+                    dc.SaveChanges();
+                    ViewBag.Message = "Property updated";
+
+                }
+            }
+            return RedirectToAction("Listings");
+        }
+        [HttpGet]
+        public ActionResult DeleteListing([Required] int property_id)
+        {
+            using (var dc = new KodishaEntities2())
+            {
+                Property prop = dc.Properties.Find(property_id);
+                if (prop != null)
+                {
+                    dc.Properties.Remove(prop);
+                    dc.SaveChanges();
+                    ViewBag.Message = "Property deleted";
+                }
+                else
+                {
+                    ViewBag.Message = "Property not found";
+                }
+            }
+
+            return RedirectToAction("Listings");
+        }
+
+        public ActionResult EditDetail()
         {
             return View();
         }
     }
+
 }
